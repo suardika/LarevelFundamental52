e884e58ae6759df2e385419d24b046d36b24a763
suardika <agratitudesign@gmail.com>
2016-06-24 14:42:08 +0800

    5 Blade - If Statement

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('content')
+    <h1> Contact Me!</h1>
+@stop
+
+@section('footer')
+    <script type="text/javascript" src="{{ asset('assets/js/jquery-1.12.4.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script>alert('contact form scripts');</script>
+@stop

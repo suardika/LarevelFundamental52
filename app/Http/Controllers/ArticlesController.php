c254d5e05971d422d63c3a9cb3b5250fe9ebe1b0
suardika <agratitudesign@gmail.com>
2016-06-28 14:50:53 +0800

    12 form requests & controller validation

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -14,47 +14,13 @@ class ArticlesController extends Controller
 {
 	public function index()
 	{
-		// return 'get all articles';
-		// $articles = \App\Article::all();
-//		$articles = Article::all();
-//		return $articles;
-
-//		$articles = Article::all();
-//		// return view ('articles.index')->with('articles', $articles);
-//		 return view ('articles.index', compact('articles'));
-
-//		$articles = Article::order_by('published_at', 'desc')->get();
-//		$articles = Article::latest('published_at')->get();
-//		return view ('articles.index', compact('articles'));
-
-//		$articles = Article::latest('published_at')->where('published_at', '<=', Carbon::now())->get();
 		$articles = Article::latest('published_at')->published()->get();
-//		$articles = Article::latest('published_at')->unpublished()->get();
 		return view ('articles.index', compact('articles'));
-
 	}
 
 	public function show($id)
 	{
-//		$article = Article::find($id);
-//		dd($article);
-//		return $article;
-
-//		$article = Article::find($id);
-//		if (is_null($article)) {
-//			abort(404);
-//		}
-//		return view('articles.show', compact('article'));
 		$article = Article::findOrFail($id);
-//		dd($article->published_at);
-//		dd($article->created_at);
-//		dd($article->created_at->year);
-//		dd($article->created_at->month);
-//		dd($article->created_at->addDays(8));
-//		dd($article->created_at->format('Y-m'));
-//		dd($article->created_at->addDays(8)->diffForHumans());
-//		dd($article->updated_at->addDays(8)->diffForHumans());
-		dd($article->published_at);
 		return view('articles.show', compact('article'));
 	}
 
@@ -65,11 +31,6 @@ class ArticlesController extends Controller
 
 	public function store()
 	{
-//		$input = Request::all();
-//		$input['published_at'] = Carbon::now();
-//		Article::create($input);
-//		return redirect('articles');
-
 		Article::create(Request::all());
 		return redirect('articles');
 	}

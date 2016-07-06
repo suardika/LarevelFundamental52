ee737df40d4cd537ff87a9e043de96b8d6e45dbd
suardika <agratitudesign@gmail.com>
2016-07-06 22:26:25 +0800

    25 - Using Custom Service Provider

--- app/Providers/ViewComposerServiceProvider.php
+++ app/Providers/ViewComposerServiceProvider.php
@@ -36,10 +36,10 @@ class ViewComposerServiceProvider extends ServiceProvider
 
     private function composeNavigation()
     {
-//        view()->composer('partials.nav', function ($view) {
-//            $view->with('latest', Article::latest()->first());
-//        });
+        view()->composer('partials.nav', function ($view) {
+            $view->with('latest', Article::latest()->first());
+        });
 
-        view()->composer('partials.nav', 'App\Http\Composers\NavigationComposer');
+//        view()->composer('partials.nav', 'App\Http\Composers\NavigationComposer');
     }
 }
